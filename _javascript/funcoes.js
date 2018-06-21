c711c0c37e47d4e307eb5176b387c6d60e2d5a99
Júnior Theodoro <juninho_lpisrock@hotmail.com>
2018-06-21 17:33:50 -0300

    Mudança gerar senha
    finalizando o site, passando map em GERAR SENHA

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,9 +1,9 @@
 function optionCheck(){
     var option = document.getElementById("senha").value;
-    if(option == "1"){
+    if(option == "MG"){
         document.getElementById("hiddenDiv").style.visibility ="visible";
     }
-    if(option != "1"){
+    if(option != "MG"){
         document.getElementById("hiddenDiv").style.visibility ="hidden";
     }
 }
